eb0c6bcb54b60c88d7567f923104c8d391884a41
Phu Le <lqp.2792@gmail.com>
2014-07-30 15:54:53 +0700

    Use Partials in web

--- Meadowlark.js
+++ Meadowlark.js
@@ -1,21 +1,71 @@
-var express = require('express');
 var fortune = require('./lib/fortune.js');
-var app = express();
-var handlebars = require('express3-handlebars');
-app.engine('hbs', handlebars({extname:'hbs', defaultLayout: 'main.hbs'}));
+var express = require('express'),
+    handlebars = require('express3-handlebars'),
+    app = express(),
+    hbs;
+hbs = handlebars.create(
+    {
+        extname:'.hbs',
+        defaultLayout: 'main.hbs'
+    }
+);
+hbs.loadPartials(function(err, partials) {
+    console.log(partials);
+});
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
+
 app.set('port', process.env.PORT || 3000);
+
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)'
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)'
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C'
+            }
+        ]
+    };
+}
+
 
 //turn on/off test
 app.use(function(request, response, next) {
+    if(!response.locals.partials) response.locals.partials = {};
+    response.locals.partials.weather = getWeatherData();
     response.locals.showTests = app.get('env') !== 'production' && request.query.test == '1';
     next();
 });
+
 // routes
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
     response.render('home');
+});
+
+app.get('/headers', function(request, response) {
+    response.set('Content-type', 'text/plain');
+    var s = '';
+    for(var name in request.headers) s+= name + ':' + request.headers[name] + '\n';
+    response.send(s);
 });
 
 app.get('/about', function(request, response) {
